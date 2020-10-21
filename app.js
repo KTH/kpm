f6d90b145595daa0239ca405ddf6d28a1aab9df2
Johan Öhman Saldes <johan.saldes@gmail.com>
2020-10-21 15:21:49 +0200

    added more blocks

--- app.js
+++ app.js
@@ -13,12 +13,28 @@ const log = require('skog')
 const express = require('express')
 const app = express()
 
-const template = handlebars.compile(fs.readFileSync(path.resolve(__dirname, './src/index.html'), {
+const blocks = {
+  title: '1.260060',
+  megaMenu: '1.855134',
+  secondaryMenu: '1.865038',
+  image: '1.77257',
+  footer: '1.202278',
+  search: '1.77262',
+  language: {
+    en: '1.77273',
+    sv: '1.272446',
+  },
+  analytics: '1.464751',
+  gtmAnalytics: '1.714097',
+  gtmNoscript: '1.714099',
+}
+
+const template = handlebars.compile(fs.readFileSync(path.resolve(__dirname, './src/index.handlebars'), {
   encoding: 'utf-8'
 }))
 
-async function fetchFooter() {
-  const res = await got.get('https://www.kth.se/cm/1.202278')
+async function fetchBlock(str) {
+  const res = await got.get(`https://www.kth.se/cm/${blocks[str]}`)
   return res.body
 }
 
@@ -30,9 +46,13 @@ app.get('/kpm/_monitor', (req, res) => {
 })
 
 app.get('/kpm', async (req, res) => {
-  const footer = await fetchFooter()
+  const footer = await fetchBlock('footer')
+  const megaMenu = await fetchBlock('megaMenu')
+  const search = await fetchBlock('search')
   res.send(template({
     footer,
+    megaMenu,
+    search,
   }));
 });
 
