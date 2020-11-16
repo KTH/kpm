9a9104135446a5ec0e297d93da8d7724a016e2d7
Carlos Saito <carlos@saito.style>
2020-11-16 15:46:43 +0100

    Implement redirect-after-login logic

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const crypto = require("crypto");
 const session = require("cookie-session");
 const got = require("got");
-
+const { validate, parse } = require("fast-xml-parser");
 const { addDays } = require("date-fns");
 
 require("dotenv").config();
@@ -16,7 +16,6 @@ require("skog/bunyan").createLogger({
 });
 const log = require("skog");
 const express = require("express");
-const authenticationMiddleware = require("./middleware/authenticationMiddleware.js");
 const app = express();
 
 const blocks = {
@@ -82,8 +81,6 @@ app.use(
 
 app.use("/kpm/dist", express.static("dist"));
 
-app.use(authenticationMiddleware);
-
 app.get("/kpm/_monitor", (req, res) => {
   res.setHeader("Content-Type", "text/plain");
   let version = "unknown";
@@ -106,10 +103,6 @@ app.get("/kpm/kpm.js", async (req, res) => {
   const baseUrl = `${process.env.SERVER_HOST_URL}/kpm`;
   const cssUrl = `${baseUrl}/${menuCssName}`;
 
-  const loginUrl = new URL(`${process.env.SSO_ROOT_URL}/login`);
-
-  loginUrl.searchParams.set("service", baseUrl);
-
   res.setHeader("Content-type", "application/javascript");
 
   if (req.session.userId) {
@@ -118,17 +111,86 @@ app.get("/kpm/kpm.js", async (req, res) => {
         baseUrl,
         cssUrl,
         userName: req.session.userId,
-        loginUrl,
+        loginUrl: baseUrl,
       })
     );
   } else {
     res.send(
       loggedOutTemplate({
-        baseUrl,
         cssUrl,
-        loginUrl,
+        loginUrl: `${baseUrl}/login`,
       })
     );
+  }
+});
+
+// Redirect the user to the KTH login server
+app.get("/kpm/login", (req, res) => {
+  const serviceUrl = new URL(
+    `${process.env.SERVER_HOST_URL}/kpm/login/callback`
+  );
+  serviceUrl.searchParams.set("next", req.query.next);
+
+  const loginUrl = new URL(`${process.env.SSO_ROOT_URL}/login`);
+  loginUrl.searchParams.set("service", serviceUrl);
+
+  log.info(`Redirect user to ${loginUrl}`);
+  res.redirect(loginUrl);
+});
+
+// User is redirected from KTH login server to this endpoint.
+// Query parameters: ticket, next
+//
+// Sets the session cookie and then redirect the user to "next"
+app.get("/kpm/login/callback", async (req, res) => {
+  try {
+    if (!req.session.userId) {
+      const serviceUrl = new URL(
+        `${process.env.SERVER_HOST_URL}/kpm/login/callback`
+      );
+      serviceUrl.searchParams.set("next", req.query.next);
+
+      const serviceValidateUrl = new URL(
+        `${process.env.SSO_ROOT_URL}/serviceValidate`
+      );
+
+      log.info(serviceUrl);
+
+      if (req.query.ticket) {
+        serviceValidateUrl.searchParams.set("ticket", req.query.ticket);
+        serviceValidateUrl.searchParams.set("service", serviceUrl);
+
+        const { body } = await got(serviceValidateUrl);
+        const parsedBody = parse(body);
+
+        log.info(body);
+
+        if (
+          validate(body) === true &&
+          !parsedBody["cas:serviceResponse"]["cas:authenticationFailure"]
+        ) {
+          log.info("Helllloooo");
+          req.session.userId =
+            parsedBody["cas:serviceResponse"]["cas:authenticationSuccess"][
+              "cas:user"
+            ];
+        }
+      }
+    }
+  } catch (err) {
+    log.error(err);
+  } finally {
+    if (!req.query.next) {
+      res.redirect("/kpm");
+      return;
+    }
+    // Avoid loops
+    if (req.query.next.endsWith("/kpm/login/callback")) {
+      res.redirect("/kpm");
+      return;
+    }
+
+    res.redirect(req.query.next);
   }
 });
 
