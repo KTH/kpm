ace399c42ee959c935ba6a9ed7ba4796f1e2f058
Johan Öhman Saldes <johan.saldes@gmail.com>
2020-11-16 11:34:54 +0100

    Added try/catch to auth middleware

--- src/middleware/authenticationMiddleware.js
+++ src/middleware/authenticationMiddleware.js
@@ -1,32 +1,37 @@
 const got = require("got");
 const { validate, parse } = require("fast-xml-parser");
+const log = require("skog");
 
 async function authenticationMiddleware(req, _res, next) {
-  if (!req.session.userId) {
-    const baseUrl = `${process.env.SERVER_HOST_URL}/kpm`;
-    const serviceValidateUrl = new URL(
-      `${process.env.SSO_ROOT_URL}/serviceValidate`
-    );
+  try {
+    if (!req.session.userId) {
+      const baseUrl = `${process.env.SERVER_HOST_URL}/kpm`;
+      const serviceValidateUrl = new URL(
+        `${process.env.SSO_ROOT_URL}/serviceValidate`
+      );
 
-    if (req.query.ticket) {
-      serviceValidateUrl.searchParams.set("ticket", req.query.ticket);
-      serviceValidateUrl.searchParams.set("service", baseUrl);
+      if (req.query.ticket) {
+        serviceValidateUrl.searchParams.set("ticket", req.query.ticket);
+        serviceValidateUrl.searchParams.set("service", baseUrl);
 
-      const { body } = await got(serviceValidateUrl);
-      const parsedBody = parse(body);
+        const { body } = await got(serviceValidateUrl);
+        const parsedBody = parse(body);
 
-      if (
-        validate(body) === true &&
-        !parsedBody["cas:serviceResponse"]["cas:authenticationFailure"]
-      ) {
-        req.session.userId =
-          parsedBody["cas:serviceResponse"]["cas:authenticationSuccess"][
-            "cas:user"
-          ];
+        if (
+          validate(body) === true &&
+          !parsedBody["cas:serviceResponse"]["cas:authenticationFailure"]
+        ) {
+          req.session.userId =
+            parsedBody["cas:serviceResponse"]["cas:authenticationSuccess"][
+              "cas:user"
+            ];
+        }
       }
     }
+    next();
+  } catch (err) {
+    log.error(err);
   }
-  next();
 }
 
 module.exports = authenticationMiddleware;
