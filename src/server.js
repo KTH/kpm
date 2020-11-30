1fd5ecd12212b733df65f3bb5e4882baa6e251a4
Carlos Saito <carlos@saito.style>
2020-11-30 15:14:03 +0100

    Replace expires with maxAge

--- src/server.js
+++ src/server.js
@@ -68,8 +68,6 @@ async function fetchBlock(str) {
 
 app.set("trust proxy", 1);
 
-var expires = new Date(Date.now() + 60 * 60 * 1000); // TODO: is 1h fine?
-
 app.use(
   session({
     name: "kpm",
@@ -77,7 +75,7 @@ app.use(
     cookie: {
       secure: !isDev,
       httpOnly: !isDev,
-      expires,
+      maxAge: 60 * 60 * 1000, // 1 hour
       domain: "kth.se",
     },
   })
