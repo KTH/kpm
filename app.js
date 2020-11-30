cf6c3b99f5adfdee105957398522920421570bce
Carlos Saito <carlos@saito.style>
2020-11-30 13:44:10 +0100

    Use envvar to show initial URL

--- app.js
+++ app.js
@@ -11,7 +11,5 @@ require("@kth/reqvars").check();
 const server = require("./server/server");
 const log = require("skog");
 server.listen(process.env.PORT || 3000, () => {
-  log.info(
-    "Starting KPM at http://localhost:3000/kpm or http://localdev.kth.se:3000/kpm"
-  );
+  log.info(`Starting KPM at ${process.env.SERVER_HOST_URL}/kpm`);
 });
