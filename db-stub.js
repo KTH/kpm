4bbea204b67a79f51285ef25c8398aa3c43e990b
Emil Stenberg <esandin@gmail.com>
2021-02-25 09:37:28 +0100

    format

--- db-stub.js
+++ db-stub.js
@@ -10,14 +10,16 @@ module.exports = {
           sv: "Introduktion till Arkitekturämnet",
           en: "Introduction to the Discipline of Architecture",
         },
-        coursePMLink: 'http://www.example.com',
-        courseRounds: [{
-          startYear: '2020',
-          startTerm: 'HT',
-          roundId: 1,
-          ladok_uid:	"cdca7048-0b8a-11ea-b635-76a5f35c4a62",
-          canvasLink: 'https://canvas.kth.se/courses/19784'
-        }]
+        coursePMLink: "http://www.example.com",
+        courseRounds: [
+          {
+            startYear: "2020",
+            startTerm: "HT",
+            roundId: 1,
+            ladok_uid: "cdca7048-0b8a-11ea-b635-76a5f35c4a62",
+            canvasLink: "https://canvas.kth.se/courses/19784",
+          },
+        ],
       },
     ],
   },
