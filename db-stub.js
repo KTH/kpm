87d4f1a18c761ab03a7066be5ad951eca4caeea6
Emil Stenberg <esandin@gmail.com>
2021-02-25 09:32:36 +0100

    added first draft of database stub

---
+++ db-stub.js
@@ -0,0 +1,24 @@
+module.exports = {
+  u1znmoik: {
+    kthid: "u1znmoik",
+    givenName: "Emil",
+    surName: "Stenberg",
+    courses: [
+      {
+        courseCode: "A11IAA",
+        ladokId: "123123123123123123123",
+        name: {
+          sv: "Introduktion till Arkitekturämnet",
+          en: "Introduction to the Discipline of Architecture",
+        },
+        coursePMLink: 'http://www.example.com',
+        courseRounds: [{
+          startYear: '2020',
+          startTerm: 'HT',
+          roundId: 1,
+          canvasLink: 'https://canvas.kth.se/courses/19784'
+        }]
+      },
+    ],
+  },
+};
