bb4c67df8836d69a1d7423e42f9551086d10227d
Emil Stenberg <esandin@gmail.com>
2021-02-26 13:53:06 +0100

    more db stubs

--- db-stub.js
+++ db-stub.js
@@ -11,14 +11,12 @@ module.exports = {
           sv: "Jättematematik-kursen!",
           en: "A giant math course",
         },
-        coursePMLink: "http://www.example.com",
       },{
         courseCode: "A11IAA",
         name: {
           sv: "Introduktion till Arkitekturämnet",
           en: "Introduction to the Discipline of Architecture",
         },
-        coursePMLink: "http://www.example.com",
       },
 
     ],
@@ -26,19 +24,18 @@ module.exports = {
     activeStudentCourses: [
       {
         courseCode: "A11IAA",
-        status: ['ANTAGEN', 'REGISTRERAD'],
         name: {
           sv: "Introduktion till Arkitekturämnet",
           en: "Introduction to the Discipline of Architecture",
         },
-        coursePMLink: "http://www.example.com",
         courseRounds: [
           {
+            status: ['ANTAGEN', 'REGISTRERAD'],
             startYear: "2020",
             startTerm: "HT",
             roundId: 1,
             ladok_uid: "cdca7048-0b8a-11ea-b635-76a5f35c4a62",
-            canvasLink: "https://canvas.kth.se/courses/19784",
+            canvas: [{url:"https://canvas.kth.se/courses/19784", published: true}], 
           },
         ],
       },
