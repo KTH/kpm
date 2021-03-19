93a822979b5a48eda19cc92bd33741d1aff5a1e7
Rasmus Kaj <kaj@kth.se>
2021-03-19 15:03:24 +0100

    Look at groups for registered courses.

--- server/openid/extract-info.js
+++ server/openid/extract-info.js
@@ -44,21 +44,86 @@ async function lookupCourseData(courseCode) {
   }
 }
 
+function addActive(buildActive, { courseCode, year, term, round, status }) {
+  log.info({ courseCode, year, term, round }, "Registrerad");
+  const roundStr = `${year}-${term}-${round || "x"}`;
+  let course;
+  if (buildActive.has(courseCode)) {
+    course = buildActive.get(courseCode);
+  } else {
+    course = new Map();
+    buildActive.set(courseCode, course);
+  }
+  if (course.has(roundStr)) {
+    course.get(roundStr).status.add(status);
+  } else {
+    course.set(roundStr, {
+      status: new Set([status]),
+      startYear: year,
+      startTerm: term,
+      roundId: round,
+    });
+  }
+}
+
 module.exports = async function extractInfoFromToken(token) {
   const completedStudentCourses = [];
-  const activeStudentCourses = []; // TODO
-  const COMPLETED_COURSE_REGEX = /^ladok2\.kurser\.(\w+)\.(\w+)\.godkand$/;
-
+  const buildActive = new Map();
+  const LADOK_COURSE_REGEX = /^ladok2\.kurser\.(\w+)\.(\w+)\.(.*)$/;
+  const REG_ROUND_REGEX = /^registrerade_(\d{4})(\d)\.(\w+)$/;
+  // Note: I'm not sure we should look at REG_TERM_REGEX at all.
+  // The relevant groups should have course rounds.
+  const REG_TERM_REGEX = /^(om)?registrerade_(\d{4})(\d)$/;
   for (const group of token.memberOf) {
-    const match = COMPLETED_COURSE_REGEX.exec(group);
-
+    const match = LADOK_COURSE_REGEX.exec(group);
     if (match) {
       const courseCode = `${match[1]}${match[2]}`;
-      const courseData = await lookupCourseData(courseCode);
-      completedStudentCourses.push(courseData);
+      if (match[3] == "godkand") {
+        const courseData = await lookupCourseData(courseCode);
+        completedStudentCourses.push(courseData);
+      } else {
+        const regmatch = REG_ROUND_REGEX.exec(match[3]);
+        if (regmatch) {
+          addActive(buildActive, {
+            courseCode,
+            year: regmatch[1],
+            term: regmatch[2],
+            round: regmatch[3],
+            status: "REGISTRERAD",
+          });
+        } else {
+          const regmatch = REG_TERM_REGEX.exec(match[3]);
+          if (regmatch) {
+            addActive(buildActive, {
+              courseCode,
+              year: regmatch[2],
+              term: regmatch[3],
+              status: "REGISTRERAD",
+            });
+          } else {
+            log.info({ courseCode, sub: match[3] }, "TODO");
+          }
+        }
+      }
     }
   }
 
+  const activeStudentCourses = [];
+  for (const [courseCode, data] of buildActive) {
+    const courseData = await lookupCourseData(courseCode);
+    courseData.courseRounds = [...data.values()].map((value) => {
+      const t = {
+        status: [...value.status],
+        startYear: value.startYear,
+        startTerm: value.startTerm,
+        roundId: value.roundId,
+        canvas: [], // TODO?
+      };
+      return t;
+    });
+    activeStudentCourses.push(courseData);
+  }
+  log.info({ activeStudentCourses }, "Collected active courses");
   return {
     fullName: token.unique_name[0],
     completedStudentCourses,
