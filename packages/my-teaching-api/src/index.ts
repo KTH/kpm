743c578700a3036b3e8dcb7f83f1554bfc3a9449
Rasmus Kaj <kaj@kth.se>
2022-09-13 14:40:08 +0200

    Make stuff prettier.

--- packages/my-teaching-api/src/index.ts
+++ packages/my-teaching-api/src/index.ts
@@ -1,32 +1,32 @@
 /* Top-level source file for my-teaching-api */
-import { config } from 'dotenv';
-config()
+import { config } from "dotenv";
+config();
 
-import express from 'express';
+import express from "express";
 
 const app = express();
-const port = parseInt(process.env.PORT || '3000');
-const prefix = process.env.PREFIX || '/kpm/teaching';
+const port = parseInt(process.env.PORT || "3000");
+const prefix = process.env.PREFIX || "/kpm/teaching";
 
 const api = express.Router();
 
-api.get('/', (req, res) => {
-    res.send({ msg: 'Hello World!!!' });
-})
-
-api.get('/mine', (req, res) => {
-    res.send({ msg: 'Not implemented yet.' });
+api.get("/", (req, res) => {
+  res.send({ msg: "Hello World!!!" });
 });
-api.get('/user/:user', (req, res) => {
-    res.send({ msg: `todo: data for ${req.params.user}` });
+
+api.get("/mine", (req, res) => {
+  res.send({ msg: "Not implemented yet." });
+});
+api.get("/user/:user", (req, res) => {
+  res.send({ msg: `todo: data for ${req.params.user}` });
 });
 
 app.use((req, res, next) => {
-    next();
-    console.log(`${req.path} => ${res.statusCode}`);
+  next();
+  console.log(`${req.path} => ${res.statusCode}`);
 });
 app.use(prefix, api);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-})
+  console.log(`Example app listening on port ${port}`);
+});
