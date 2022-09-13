c979fc271d0d04b5d3598dc44f9f2e252989508d
Rasmus Kaj <kaj@kth.se>
2022-09-13 14:11:22 +0200

    Use dotenv.

--- packages/my-teaching-api/src/index.ts
+++ packages/my-teaching-api/src/index.ts
@@ -1,3 +1,16 @@
 /* Top-level source file for my-teaching-api */
+import { config } from 'dotenv';
+config()
 
-console.log("Hello world!")
+import express from 'express';
+
+const app = express();
+const port = parseInt(process.env.PORT || '3000');
+
+app.get('/', (req, res) => {
+    res.send({msg: 'Hello World!!!'});
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})
