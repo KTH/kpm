39b13bc88fc36cbeaf5ce3c1677cdc5090706d44
Rasmus Kaj <kaj@kth.se>
2022-09-16 13:35:54 +0200

    Add my-canvas-rooms-api outline.

---
+++ packages/my-canvas-rooms-api/src/index.ts
@@ -0,0 +1,32 @@
+/* Top-level source file for my-canvas-rooms-api */
+import { config } from "dotenv";
+config();
+
+import express from "express";
+
+const app = express();
+const port = parseInt(process.env.PORT || "3000");
+const prefix = process.env.PREFIX || "/kpm/canvas-rooms";
+
+const api = express.Router();
+
+api.get("/", (req, res) => {
+  res.send({ msg: "Hello World!!!" });
+});
+
+api.get("/mine", (req, res) => {
+  res.send({ msg: "Not implemented yet." });
+});
+api.get("/user/:user", (req, res) => {
+  res.send({ msg: `todo: data for ${req.params.user}` });
+});
+
+app.use((req, res, next) => {
+  next();
+  console.log(`${req.path} => ${res.statusCode}`);
+});
+app.use(prefix, api);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
