88f19509fd75306509a1ae8ed3d066aa4473a9d6
Sebastian Ware <sebastian@urbantalk.se>
2022-09-21 13:35:26 +0200

    So we need to run a shell script to generate the package-lock.json files used in our docker image

---
+++ bin/publish.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Send a list of workspace folder names as arguments
+workspaces="$@"
+
+# Hide package.json from NPM
+mv package.json package.json.tmp
+
+# Generate package-lock.json files
+for d in $workspaces
+do
+    (cd packages/$d && npm i --package-lock-only)
+done
+
+# Reveal package.json to NPM
+mv package.json.tmp package.json
