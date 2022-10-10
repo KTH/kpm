35c132cee43f192edc76f86f6230b715578061dd
Sebastian Ware <sebastian@urbantalk.se>
2022-10-10 15:42:04 +0200

    Added tests and broke deconstructor into two functions because it is easier to read.

---
+++ apis/my-studies-api/__test__/convertToCoursesProgrammes.spec.ts
@@ -0,0 +1,107 @@
+import { describe, expect, jest, test } from '@jest/globals';
+import { convertToCourses, convertToProgrammes } from "../src";
+
+// ladok2.kurser.SE.1020
+// ladok2.kurser.ME.2053.antagna_20222
+// ladok2.kurser.MF.2102.godkand
+// ladok2.kurser.MF.2114
+// ladok2.kurser.MF.2032.registrerade_20221.1
+// ladok2.kurser.MF.2039.registrerade_20212.1
+// ladok2.program.TIPDM.registrerade_20221
+// ladok2.kurser.ÅF.2102.godkand
+// ladok2.kurser.ÅF.210v.godkand
+
+const courseTestCases: {[index: string]: any } = {
+  "ladok2.kurser.SE.1020": {
+    "type": "kurser",
+    "code": "SE1020",
+    "code_pt1": "SE",
+    "code_pt2": "1020"
+  },
+  "ladok2.kurser.ME.2053.antagna_20222": {
+    "type": "kurser",
+    "code": "ME2053",
+    "code_pt1": "ME",
+    "code_pt2": "2053",
+    "status": "antagna",
+    "year": "2022",
+    "term": "2",
+  },
+  "ladok2.kurser.MF.2102.godkand": {
+    "type": "kurser",
+    "code": "MF2102",
+    "code_pt1": "MF",
+    "code_pt2": "2102",
+    "status": "godkand",
+  },
+  "ladok2.kurser.MF.2114": {
+    "type": "kurser",
+    "code": "MF2114",
+    "code_pt1": "MF",
+    "code_pt2": "2114"
+  },
+  "ladok2.kurser.MF.2032.registrerade_20221.1": {
+    "type": "kurser",
+    "code": "MF2032",
+    "code_pt1": "MF",
+    "code_pt2": "2032",
+    "status": "registrerade",
+    "year": "2022",
+    "term": "1",
+    "round": "1"
+  },
+  "ladok2.kurser.MF.2039.registrerade_20212.1": {
+    "type": "kurser",
+    "code": "MF2039",
+    "code_pt1": "MF",
+    "code_pt2": "2039",
+    "status": "registrerade",
+    "year": "2021",
+    "term": "2",
+    "round": "1"
+  },
+  "ladok2.kurser.ÅF.2102.godkand": {
+    "type": "kurser",
+    "code": "ÅF2102",
+    "code_pt1": "ÅF",
+    "code_pt2": "2102",
+    "status": "godkand",
+  },
+  "ladok2.kurser.ÅF.210v.godkand": {
+    "type": "kurser",
+    "code": "ÅF210v",
+    "code_pt1": "ÅF",
+    "code_pt2": "210v",
+    "status": "godkand",
+  },
+}
+
+const programmeTestCases: {[index: string]: any }  = {
+  "ladok2.program.TIPDM.registrerade_20221": {
+    "type": "program",
+    "code": "TIPDM",
+    "status": "registrerade",
+    "year": "2022",
+    "term": "1",
+  },
+}
+
+describe("UG REST API response should detect courses", () => {
+  for (const testCase of Object.keys(courseTestCases)) {
+    test(testCase, () => {
+      const tmp = convertToCourses([testCase]);
+      const res = JSON.parse(JSON.stringify(tmp[0]));
+      expect(res).toStrictEqual(courseTestCases[testCase]);
+    })
+  }
+})
+
+describe("UG REST API response should detect programmes", () => {
+  for (const testCase of Object.keys(programmeTestCases)) {
+    test(testCase, () => {
+      const tmp = convertToProgrammes([testCase]);
+      const res = JSON.parse(JSON.stringify(tmp[0]));
+      expect(res).toStrictEqual(programmeTestCases[testCase]);
+    })
+  }
+})
--- apis/my-studies-api/src/index.ts
+++ apis/my-studies-api/src/index.ts
@@ -73,26 +73,95 @@ api.get("/user/:user", async (req, res) => {
     }
   }
 
-  /*
-    Don't panic, it is just a regex! :D The trick is optional match groups (...)? and optional matches |
-    This allows us to both match program and kurser but also different lengths of kurser.
-    Use https://regex101.com/ to investigate, debug or extend.
+  //const outp = convertToCoursesProgrammes(json!);
+  res.status(statusCode || 200).send({
+    courses: convertToCourses(json!.map(o => o.name)),
+    programmes: convertToProgrammes(json!.map(o => o.name)),
+  });
+});
 
-    Tested matches for regex check:
-    ladok2.kurser.SE.1020
-    ladok2.kurser.ME.2053.antagna_20222
-    ladok2.kurser.MF.2102.godkand
-    ladok2.kurser.MF.2114
-    ladok2.kurser.MF.2032.registrerade_20221.1
-    ladok2.kurser.MF.2039.registrerade_20212.1
-    ladok2.program.TIPDM.registrerade_20221
-    ladok2.kurser.ÅF.2102.godkand
-    ladok2.kurser.ÅF.210v.godkand
-  */
-  // TODO: Move the code to a function and write tests for different types of values (do the same in my-teaching-api)
-  const nameRegex = /^ladok2\.(?<type>[^\.]*)\.(?<code_pt1>[^\.]*)\.(((?<pstatus>[^\._]*)_(?<pyear>\d{4})(?<pterm>\d{1}))|(?<code_pt2>[^\.]*)(\.(?<cstatus>[^\._]*)(_(?<cyear>\d{4})(?<cterm>\d{1})(\.(?<cterm_pt2>\d{1}))?)?)?)$/i
-  const tmpJson = json?.map(o => o.name.match(nameRegex)?.groups).filter(o => o);
-  const courses = tmpJson?.filter(o => o?.type === "kurser").map((o: any) => {
+app.use((req, res, next) => {
+  next();
+  console.log(`${req.path} => ${res.statusCode}`);
+});
+app.use(prefix, api);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+
+// export function convertToCoursesProgrammes(inp: any[]) {
+//   /*
+//     Don't panic, it is just a regex! :D The trick is optional match groups (...)? and optional matches |
+//     This allows us to both match program and kurser but also different lengths of kurser.
+//     Use https://regex101.com/ to investigate, debug or extend.
+
+//     Tested matches for regex check:
+//     ladok2.kurser.SE.1020
+//     ladok2.kurser.ME.2053.antagna_20222
+//     ladok2.kurser.MF.2102.godkand
+//     ladok2.kurser.MF.2114
+//     ladok2.kurser.MF.2032.registrerade_20221.1
+//     ladok2.kurser.MF.2039.registrerade_20212.1
+//     ladok2.program.TIPDM.registrerade_20221
+//     ladok2.kurser.ÅF.2102.godkand
+//     ladok2.kurser.ÅF.210v.godkand
+//   */
+//   // TODO: Move the code to a function and write tests for different types of values (do the same in my-teaching-api)
+//   const nameRegex = /^ladok2\.(?<type>[^\.]*)\.(?<code_pt1>[^\.]*)\.(((?<pstatus>[^\._]*)_(?<pyear>\d{4})(?<pterm>\d{1}))|(?<code_pt2>[^\.]*)(\.(?<cstatus>[^\._]*)(_(?<cyear>\d{4})(?<cterm>\d{1})(\.(?<cterm_pt2>\d{1}))?)?)?)$/i
+//   const tmpJson = inp?.map(o => o.name.match(nameRegex)?.groups).filter(o => o);
+//   const courses = tmpJson?.filter(o => o?.type === "kurser").map((o: any) => {
+//     const { type, code_pt1, code_pt2, cstatus, cyear, cterm, cterm_pt2 } = o;
+//     return {
+//       type,
+//       code: `${code_pt1}${code_pt2}`,
+//       code_pt1,
+//       code_pt2,
+//       status: cstatus,
+//       year: cyear,
+//       term: cterm,
+//       round: cterm_pt2 // QUESTION: Is this really round id or perhaps section or something similar? Matches the last number of "registrerade_20221.1"
+//     }
+//   });
+//   const programmes = tmpJson?.filter(o => o?.type === "program").map((o: any) => {
+//     const { type, code_pt1, pstatus, pyear, pterm } = o;
+//     return {
+//       type,
+//       code: code_pt1,
+//       status: pstatus,
+//       year: pyear,
+//       term: pterm
+//     }
+//   });
+
+//   return {
+//     programmes,
+//     courses,
+//   }
+// }
+
+/*
+  Don't panic, it is just a regex! :D The trick is optional match groups (...)? and optional matches |
+  This allows us to both match program and kurser but also different lengths of kurser.
+  Use https://regex101.com/ to investigate, debug or extend.
+
+  Tested matches for regex check:
+  ladok2.kurser.SE.1020
+  ladok2.kurser.ME.2053.antagna_20222
+  ladok2.kurser.MF.2102.godkand
+  ladok2.kurser.MF.2114
+  ladok2.kurser.MF.2032.registrerade_20221.1
+  ladok2.kurser.MF.2039.registrerade_20212.1
+  ladok2.program.TIPDM.registrerade_20221
+  ladok2.kurser.ÅF.2102.godkand
+  ladok2.kurser.ÅF.210v.godkand
+*/
+const nameRegex = /^ladok2\.(?<type>[^\.]*)\.(?<code_pt1>[^\.]*)\.(((?<pstatus>[^\._]*)_(?<pyear>\d{4})(?<pterm>\d{1}))|(?<code_pt2>[^\.]*)(\.(?<cstatus>[^\._]*)(_(?<cyear>\d{4})(?<cterm>\d{1})(\.(?<cterm_pt2>\d{1}))?)?)?)$/i
+
+export function convertToCourses(inp: string[]) {
+  const tmpJson = inp?.map(o => o.match(nameRegex)?.groups).filter(o => o && o.type === "kurser");
+  return tmpJson?.map((o: any) => {
     const { type, code_pt1, code_pt2, cstatus, cyear, cterm, cterm_pt2 } = o;
     return {
       type,
@@ -105,7 +174,11 @@ api.get("/user/:user", async (req, res) => {
       round: cterm_pt2 // QUESTION: Is this really round id or perhaps section or something similar? Matches the last number of "registrerade_20221.1"
     }
   });
-  const programmes = tmpJson?.filter(o => o?.type === "program").map((o: any) => {
+}
+
+export function convertToProgrammes(inp: string[]) {
+  const tmpJson = inp?.map(o => o.match(nameRegex)?.groups).filter(o => o && o.type === "program");
+  return tmpJson?.map((o: any) => {
     const { type, code_pt1, pstatus, pyear, pterm } = o;
     return {
       type,
@@ -115,19 +188,4 @@ api.get("/user/:user", async (req, res) => {
       term: pterm
     }
   });
-
-  res.status(statusCode || 200).send({
-    programmes,
-    courses,
-  })
-});
-
-app.use((req, res, next) => {
-  next();
-  console.log(`${req.path} => ${res.statusCode}`);
-});
-app.use(prefix, api);
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+}
