f1f91d194aa1448d93ba345cadfa2eaf4ce52043
Carlos Saito <carlos@saito.style>
2022-10-17 15:55:02 +0200

    Fix session

--- kpm/kpm-backend/src/index.ts
+++ kpm/kpm-backend/src/index.ts
@@ -1,6 +1,10 @@
 import "./config";
 import express from "express";
-import { loggingHandler, errorHandler, uncaughtExceptionCallback } from "kpm-api-common";
+import {
+  loggingHandler,
+  errorHandler,
+  uncaughtExceptionCallback,
+} from "kpm-api-common";
 import { name as APP_NAME } from "../package.json";
 import { api } from "./api";
 import { auth } from "./auth";
@@ -11,8 +15,10 @@ const PREFIX = process.env.PROXY_PATH_PREFIX || "/kpm";
 
 export const app = express();
 
-process.on('uncaughtException', uncaughtExceptionCallback);
+process.on("uncaughtException", uncaughtExceptionCallback);
 
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(sessionMiddleware);
 app.use(loggingHandler);
 app.use(PREFIX, auth);
--- kpm/kpm-backend/src/session.ts
+++ kpm/kpm-backend/src/session.ts
@@ -5,22 +5,22 @@ const PORT = process.env.PORT || 3000;
 const PROXY_HOST = process.env.PROXY_HOST || `http://localhost:${PORT}`;
 
 export const sessionMiddleware = expressSessio({
-    name: "kpm.sid",
-    proxy: true,
-    // store: store,
-    cookie: {
-      domain: new URL(PROXY_HOST).hostname,
-      maxAge: 14 * 24 * 3600 * 1000,
-      httpOnly: true,
-      secure: "auto",
-      sameSite: "none",
-    },
+  name: "kpm.sid",
+  proxy: true,
+  // store: store,
+  cookie: {
+    domain: new URL(PROXY_HOST).hostname,
+    maxAge: 14 * 24 * 3600 * 1000,
+    httpOnly: true,
+    // secure: "auto",
+    // sameSite: "none",
+  },
 
-    // Read more: https://www.npmjs.com/package/express-session#resave
-    resave: false,
+  // Read more: https://www.npmjs.com/package/express-session#resave
+  resave: false,
 
-    // Save only sessions when user is authenticated. Setting "saveUnitialized"
-    // to "false" prevents creation of sessions when app is accessed via API
-    saveUninitialized: false,
-    secret: SESSION_SECRET,
-  });
+  // Save only sessions when user is authenticated. Setting "saveUnitialized"
+  // to "false" prevents creation of sessions when app is accessed via API
+  saveUninitialized: false,
+  secret: SESSION_SECRET,
+});
