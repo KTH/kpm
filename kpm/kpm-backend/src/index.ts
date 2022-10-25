8ac427921a6d0c257e1776d95ee8a87571323f33
Carlos Saito <carlos@saito.style>
2022-10-25 13:19:05 +0200

    Remove redundand info

--- kpm/kpm-backend/src/index.ts
+++ kpm/kpm-backend/src/index.ts
@@ -1,7 +1,6 @@
 import "./config";
 import express from "express";
 import { loggingHandler, errorHandler } from "kpm-api-common";
-import { name as APP_NAME } from "../package.json";
 import { api } from "./api";
 import { auth } from "./auth";
 import { sessionMiddleware } from "./session";
@@ -18,8 +17,7 @@ app.use(sessionMiddleware);
 app.use(loggingHandler);
 app.use(`${PREFIX}/auth`, auth);
 app.use(`${PREFIX}/api`, api);
-app.use(errorHandler);
 
 app.listen(PORT, () => {
-  log.info(`${APP_NAME} listening on port ${PORT}`);
+  log.info(`Listening on port ${PORT}`);
 });
