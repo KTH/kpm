cffbffdf3994f5c782e6c0a64fbfbc65459cdcf0
Carlos Saito <carlos@saito.style>
2022-10-25 13:28:59 +0200

    Add skog to my-studies-api

--- apis/my-studies-api/src/config.ts
+++ apis/my-studies-api/src/config.ts
@@ -1,4 +1,35 @@
-import * as dotenv from "dotenv";
-import * as reqvars from "@kth/reqvars";
-dotenv.config();
-reqvars.check();
+/**
+ * This file initializes settings (environmental variables, skog, among other)
+ *
+ * It must be `import`-ed like this in the beginning of the app:
+ *
+ * ```
+ * import "./config/start";
+ * ```
+ */
+
+/* eslint-disable @typescript-eslint/no-var-requires */
+import log, { initializeLogger, setFields } from "skog";
+require("dotenv").config();
+initializeLogger();
+setFields({
+  app: "my-studies-api",
+});
+
+process.on("uncaughtException", (err) => {
+  log.fatal(err, "UncaughtException");
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (reason) => {
+  if (reason instanceof Error) {
+    log.fatal(reason, "UnhandledRejection");
+  } else {
+    log.fatal(
+      "UnhandledRejection: some promise have been rejected but didn't throw an `Error` object. It is not possible to show a stack trace"
+    );
+  }
+  process.exit(1);
+});
+
+require("@kth/reqvars").check();
