6ea00f4f3ec1b01ca7f0793d7dfad8295c5af93f
Carlos Saito <carlos@saito.style>
2022-10-25 11:15:14 +0200

    Add skog and update config.ts

--- kpm/kpm-backend/src/index.ts
+++ kpm/kpm-backend/src/index.ts
@@ -1,21 +1,16 @@
 import "./config";
 import express from "express";
-import {
-  loggingHandler,
-  errorHandler,
-  uncaughtExceptionCallback,
-} from "kpm-api-common";
+import { loggingHandler, errorHandler } from "kpm-api-common";
 import { name as APP_NAME } from "../package.json";
 import { api } from "./api";
 import { auth } from "./auth";
 import { sessionMiddleware } from "./session";
+import log from "skog";
 
 const PORT = parseInt(process.env.PORT || "3000");
 const PREFIX = process.env.PROXY_PATH_PREFIX || "/kpm";
 
 export const app = express();
-
-process.on("uncaughtException", uncaughtExceptionCallback);
 
 app.use(express.json());
 app.use(express.urlencoded());
@@ -26,5 +21,5 @@ app.use(`${PREFIX}/api`, api);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-  console.log(`${APP_NAME} listening on port ${PORT}`);
+  log.info(`${APP_NAME} listening on port ${PORT}`);
 });
