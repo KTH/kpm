dfd8d7d1582aa488f2a1e1998d998005f281e1c4
Sebastian Ware <sebastian@urbantalk.se>
2022-10-25 14:05:22 +0200

    Changed from custom divs to summary/detail. Reveal animations were inconsistent in behaviour with summary/detail so removed them for now.

--- kpm/kpm-frontend/src/components/groups.tsx
+++ kpm/kpm-frontend/src/components/groups.tsx
@@ -1,27 +1,13 @@
 import * as React from "react";
 
 export function CollapsableGroup({ title, children, defaultOpen = false }: any) {
-  const [open, setOpen] = React.useState(defaultOpen);
-
-  let cls = "kpm-collapsable-group";
-  if (open) {
-    cls += " open"
-  }
-
-  let clsAnim = "kpm-anim";
-  if (open) {
-    clsAnim += " open";
-  }
-
   return (
-    <div className={cls}>
-      <h2 onClick={(e) => { e.preventDefault(); setOpen(!open) }}><Caret open={open} />{title}</h2>
+    <details className="kpm-collapsable-group">
+      <summary>{title}</summary>
       <ul className="kpm-link-list">
-        <div className={clsAnim}>
-          {children}
-        </div>
+        {children}
       </ul>
-    </div>
+    </details>
   )
 }
 
@@ -30,16 +16,5 @@ export function GroupItem({ className, children }: any) {
     <li className={className}>
       {children}
     </li>
-  )
-}
-
-function Caret({ open = false }: any) {
-  let cls = "kpm-caret";
-  if (open) {
-    cls += " open";
-  }
-
-  return (
-    <svg className={cls} id="a" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 7.41 12"><path d="M0,10.58L4.58,6,0,1.41,1.41,0,7.41,6,1.41,12l-1.41-1.42Z" /></svg>
   )
 }
