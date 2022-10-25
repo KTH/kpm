29b9857556069355067e88d03865e7c9e05ec01d
Carlos Saito <carlos@saito.style>
2022-10-25 13:30:22 +0200

    Add skog to my-canvas-rooms-api

--- apis/my-canvas-rooms-api/src/index.ts
+++ apis/my-canvas-rooms-api/src/index.ts
@@ -1,11 +1,7 @@
 import "./config";
 import express from "express";
-import {
-  loggingHandler,
-  errorHandler,
-  uncaughtExceptionCallback,
-} from "kpm-api-common";
-import { name as APP_NAME } from "../package.json";
+import { loggingHandler, errorHandler } from "kpm-api-common";
+import log from "skog";
 import { api } from "./api";
 
 const PORT = parseInt(process.env.PORT || "3000");
@@ -13,11 +9,10 @@ const PREFIX = process.env.PROXY_PATH_PREFIX || "/kpm/canvas-rooms";
 
 const app = express();
 
-process.on("uncaughtException", uncaughtExceptionCallback);
 app.use(loggingHandler);
 app.use(PREFIX, api);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-  console.log(`${APP_NAME} listening on port ${PORT}`);
+  log.info(`Listening on port ${PORT}`);
 });
