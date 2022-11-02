e0ff6d827247a0c4ad7e47087669a332318d939d
Rasmus Kaj <kaj@kth.se>
2022-11-02 15:03:26 +0100

    Update type.

--- kpm/kpm-backend-interface/src/index.ts
+++ kpm/kpm-backend-interface/src/index.ts
@@ -23,6 +23,7 @@ export type TCanvasRoom = {
   type: "course" | "exam" | "rapp" | undefined;
   startTerm?: string; // YYYYn (n = 1 | 2)
   examDate?: string; // YYYY-mm-dd
+  registrationCode?: string; // Often a five-digit number, but may vary.
   favorite: boolean;
 };
 
