eb915df2feea90325d36cce82326e658b8e1b221
Rasmus Kaj <kaj@kth.se>
2022-11-08 16:36:26 +0100

    Ugly first-step frontend fix.

--- kpm/kpm-frontend/src/panes/studies.tsx
+++ kpm/kpm-frontend/src/panes/studies.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { useLoaderData } from "react-router-dom";
 import { APIStudies } from "kpm-backend-interface";
 import { MenuPane } from "../components/menu";
+import { CanvasRoomShortList } from "./teaching";
 
 import "./studies.scss";
 
@@ -19,25 +20,21 @@ export function Studies() {
     <MenuPane>
       <h2>Studies</h2>
       <ul className="kpm-studies">
-        {courses?.map((course) => {
-          const { code, status, year } = course ?? {};
-          const key = `${code}-${year}`;
-          return (
-            <Course key={key} courseCode={code} year={year} status={status} />
-          );
+        {Object.entries(courses)?.map((course_code, course) => {
+          return <Course courseCode={course_code} course={course} />;
         })}
       </ul>
     </MenuPane>
   );
 }
 
-function Course({ courseCode, year, status }: any) {
+function Course({ courseCode, course }: any) {
   return (
     <li className="kpm-studies-course">
-      <h2>{courseCode}</h2>
-      <h3>
-        {year} | {status}
-      </h3>
+      <h2>
+        {courseCode} {course.title.sv} {course.credits} {course.creditUnitAbbr}
+      </h2>
+      <CanvasRoomShortList rooms={course.rooms} />
     </li>
   );
 }
