ca56a9ab21b60d3db682b63251597423e55627ba
Sebastian Ware <sebastian@urbantalk.se>
2022-11-11 12:35:49 +0100

    Add CORS config for STAGE in kpm-backend

--- kpm/kpm-backend/src/index.ts
+++ kpm/kpm-backend/src/index.ts
@@ -6,6 +6,7 @@ import { auth } from "./auth";
 import { sessionMiddleware, cookieParserMiddleware } from "./session";
 import log from "skog";
 import { widgetJsHandler, widgetJsAssets, previewHandler } from "./widget.js";
+import cors from "cors";
 
 const IS_DEV = process.env.NODE_ENV !== "production";
 const IS_STAGE = process.env.DEPLOYMENT === "stage";
@@ -18,8 +19,14 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(sessionMiddleware);
 app.use(cookieParserMiddleware);
-
 app.use(loggingHandler);
+
+if (IS_STAGE) {
+  // We currently only need CORS in STAGE due to ref web on different domain as api
+  app.use(cors({
+    origin: 'https://www-r.referens.sys.kth.se/',
+  }))
+}
 app.use(`${PREFIX}/auth`, auth);
 app.use(`${PREFIX}/api`, api);
 app.use(`${PREFIX}/kpm.js`, widgetJsHandler);
