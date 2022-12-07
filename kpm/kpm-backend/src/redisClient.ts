9d870d47f4bdba9909e605c82a3b43fda2e47507
Sebastian Ware <sebastian@urbantalk.se>
2022-12-07 11:00:54 +0100

    Don't rethrow Redis errors and added reconnect logic because Azure can have connection disruptions

--- kpm/kpm-backend/src/redisClient.ts
+++ kpm/kpm-backend/src/redisClient.ts
@@ -1,8 +1,10 @@
 import { createClient, RedisClientType } from "redis";
+import log from "skog";
 
 const REDIS_PORT = parseInt(process.env.REDIS_PORT || "6379", 10);
 const REDIS_HOST = process.env.REDIS_HOST || "localhost";
 const REDIS_PASSWORD = process.env.REDIS_PASSWORD || "";
+const REDIS_CONNECT_TIMEOUT = parseInt(process.env.REDIS_CONNECT_TIMEOUT || "5000");
 const useRedis = !!process.env.REDIS_HOST;
 
 let redisClient: RedisClientType | undefined = undefined;
@@ -16,19 +18,32 @@ export function getRedisClient(): RedisClientType | undefined {
         port: REDIS_PORT,
         host: REDIS_HOST,
         tls: REDIS_PORT === 6380,
+        reconnectStrategy(retries) {
+          // How many ms to wait until reconnect
+          return Math.min(retries * 100, 1000);
+        },
+        connectTimeout: REDIS_CONNECT_TIMEOUT,
       },
       password: REDIS_PASSWORD,
       // Notes on legacy mode: https://github.com/tj/connect-redis/pull/337
       legacyMode: true,
     });
     redisClient.connect().catch((err) => {
-      // TODO: Error handling (e.g. wrong password)
-      throw err;
+      log.error({ message: "Redis client connection error", err });
+    });
+
+    redisClient.on("reconnecting", () => {
+      log.info({ message: "Redis client reconnecting" });
+    });
+
+    redisClient.on("ready", () => {
+      log.info({ message: "Redis client ready to go!" });
     });
 
     redisClient.on("error", (err) => {
-      // TODO: Error handling
-      throw err;
+      // Rethrowing errors here can cause redis-client v4 to give up on life
+      // https://github.com/redis/node-redis/issues/2032 
+      log.error({ message: "Redis client error", err });
     });
 
     return redisClient;
