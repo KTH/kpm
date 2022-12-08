bfbb2c72ae70f12eb1e98b61f5b0cf3b35a08d40
Sebastian Ware <sebastian@urbantalk.se>
2022-12-08 16:50:43 +0100

    Move max width to inset

--- kpm/kpm-backend/src/widget.js.ts
+++ kpm/kpm-backend/src/widget.js.ts
@@ -32,7 +32,7 @@ var cr = (t) => document.createElement(t),
 ap = (n) => document.head.appendChild(n);
 let sc = cr('script'); sc.defer = true; sc.src = js; ap(sc);
 let st = cr('link'); st.rel = "stylesheet"; st.href = css; ap(st);
-let n = cr('div'); n.id = "kpm-6cf53"; n.style = "height: calc(var(--kpm-bar-height,2em) + 1px); max-width: 1228px; margin: 0 auto; background-color: #65656c;";n.innerHtml = "";document.body.prepend(n);
+let n = cr('div'); n.id = "kpm-6cf53"; n.style = "inset: 0; position: fixed; height: calc(var(--kpm-bar-height,2em) + 1px); margin: 0 auto; background-color: #65656c;";n.innerHtml = "";document.body.prepend(n);
 window.__kpmPublicUriBase__ = "${
       publicUriBase
       // NOTE: This global variable is read in kpm-backend/src/panes/utils.ts
@@ -47,8 +47,8 @@ window.__kpmCurrentUser__ = ${
   } else {
     res.type("text/javascript").send(`(function (js, css) {
 var cr = (t) => document.createElement(t);
-let n = cr('div'); n.id = "kpm-6cf53"; n.style = "inset: 0; position: fixed; display: flex; align-items: center; height: calc(var(--kpm-bar-height,2em) + 1px); padding: 0 1rem; justify-content: end; max-width: 1228px; margin: 0 auto; background-color: #65656c;";
-n.innerHTML = "<a href='${LOGIN_URL}?nextUrl=" + location.href + "' style='color: white;'>Login</a>"; document.body.prepend(n);
+let n = cr('div'); n.id = "kpm-6cf53"; n.style = "inset: 0; position: fixed; display: flex; align-items: center; height: calc(var(--kpm-bar-height,2em) + 1px); padding: 0 1rem; justify-content: end; margin: 0 auto; background-color: #65656c;";
+n.innerHTML = "<div style='max-width: 1228px; display: flex; align-items: center;'><a href='${LOGIN_URL}?nextUrl=" + location.href + "' style='margin-left: auto; color: white;'>Login</a></div>"; document.body.prepend(n);
       })("${publicUriBase}/assets/${assets["index.js"]?.fileName}", "${publicUriBase}/assets/${assets["index.css"]?.fileName}");`);
   }
 
