61b5df3a85001d431ccc4a7a02a892bcd2b72f8d
Sebastian Ware <sebastian@urbantalk.se>
2022-12-14 18:39:06 +0100

    Final fixes to release script

--- packages/cellus-release/src/index.ts
+++ packages/cellus-release/src/index.ts
@@ -1,4 +1,5 @@
 import { execSync } from "child_process";
+import path from "path";
 import {
   loadPackageJson,
   loadDockerConf,
@@ -11,7 +12,8 @@ import {
  *************************/
 
 try {
-  const cwd = process.cwd();
+  const [a, b, ...args] = process.argv;
+  console.log(`Preparing release of: ${args.join(", ")}`);
 
   const cmdBuffer = execSync("git branch --show-current");
   const cmdOutput = await new TextDecoder().decode(cmdBuffer);
@@ -22,18 +24,26 @@ try {
     );
   }
 
-  const dockerConf = await loadDockerConf(cwd);
-  const packageJson = await loadPackageJson(cwd);
-  const currentVersion = dockerConf["IMAGE_VERSION"];
+  // TODO: We should really use the version from root package.json so everything is in sync
+  // But using last processed docker.conf for now
+  let currentVersion;
+  for (const appDir of args) {
+    const cwd = path.join(process.cwd(), appDir);
 
-  if (!/^[0-9\.]*$/.test(currentVersion)) {
-    throw new Error(
-      `The version string contains illegal characters: ${currentVersion}`
+    const dockerConf = await loadDockerConf(cwd);
+    const packageJson = await loadPackageJson(cwd);
+    const appVersion = dockerConf["IMAGE_VERSION"];
+
+    if (!/^[0-9\.]*$/.test(appVersion)) {
+      throw new Error(
+        `The version string contains illegal characters: ${appVersion}`
+      );
+    }
+    console.log(
+      `\n${packageJson.name} --- VERSION: "~${appVersion}.0" # Deploy any minor version match\n`
     );
+    currentVersion = appVersion;
   }
-  console.log(
-    `\n${packageJson.name} --- VERSION: "~${currentVersion}.0" # Deploy any minor version match\n`
-  );
 
   // Tag a release
   const tag = `v${currentVersion}`;
@@ -41,13 +51,21 @@ try {
   execSync(`git tag ${tag}`);
   execSync(`git push --atomic origin main ${tag}`);
 
-  // Update docker.conf to avoid deploying further changes to active
-  const tmpVersion = currentVersion.split(".").map((v: string) => parseInt(v));
-  tmpVersion[1]++;
-  const newVersion = tmpVersion.join(".");
-  const newDockerConf = { ...dockerConf };
-  newDockerConf["IMAGE_VERSION"] = newVersion;
-  await writeDockerConf(cwd, newDockerConf);
+  for (const appDir of args) {
+    const cwd = path.join(process.cwd(), appDir);
+
+    const dockerConf = await loadDockerConf(cwd);
+    const packageJson = await loadPackageJson(cwd);
+    const appVersion = dockerConf["IMAGE_VERSION"];
+    // Update docker.conf to avoid deploying further changes to active
+    const tmpVersion = appVersion.split(".").map((v: string) => parseInt(v));
+    tmpVersion[1]++;
+    const newVersion = tmpVersion.join(".");
+    const newDockerConf = { ...dockerConf };
+    newDockerConf["IMAGE_VERSION"] = newVersion;
+    await writeDockerConf(cwd, newDockerConf);
+  }
+
   execSync(
     "git add docker.conf && git commit -m 'Bump docker.conf for deployment to STAGE only' && git push origin main"
   );
