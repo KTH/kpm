b09c79bd917fab7040034ff001c67469b13ca695
Sebastian Ware <sebastian@urbantalk.se>
2022-12-14 14:29:44 +0100

    Improve error handler for my-canvas-rooms-api

--- apis/my-canvas-rooms-api/src/index.ts
+++ apis/my-canvas-rooms-api/src/index.ts
@@ -4,9 +4,12 @@ import { loggingHandler, errorHandler } from "kpm-api-common";
 import log from "skog";
 import { api } from "./api";
 import { status } from "./publicApi";
+import { AuthError } from "kpm-api-common/src/errors";
 
 const PORT = parseInt(process.env.PORT || "3000");
 const PREFIX = process.env.PROXY_PATH_PREFIX || "/kpm/canvas-rooms";
+
+export type APIAuthErrType = "Unauthorized";
 
 /**
  * In the other apps, unauthorized from another service would be a
@@ -15,12 +18,20 @@ const PREFIX = process.env.PROXY_PATH_PREFIX || "/kpm/canvas-rooms";
  * authentication.
  */
 function localErrorHandler(err: any, req: any, res: any, next: any) {
+  // TODO: This should really be handled at the point where the call to Canvas is made
+  // using async errorhandlers to get proper stack trace. Look at `function openIdErr(err: any)`
+  // TODO: When implemented you can switch `localErrorHandler` for `errorHandler` as error
+  // middleware
   if (err.message.startsWith("Unauthorized.")) {
-    log.info("Unauthorized.  Blaming the client");
-    res.status(403).send(err.message);
-  } else {
-    errorHandler(err, req, res, next);
+    err = new AuthError<APIAuthErrType>({
+      type: "Unauthorized",
+      message: "Client didn't provide a valid Canvas auth token",
+      details: undefined, // Do we want to log the user id?
+      err,
+    });
   }
+
+  errorHandler(err, req, res, next);
 }
 
 const app = express();
