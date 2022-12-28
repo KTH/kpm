c46a3c092b6503730482c6e173ef3f83c03a33e9
Sebastian Ware <sebastian@urbantalk.se>
2022-12-28 14:51:40 +0100

    Only mount menu in outermost frame

--- kpm/kpm-frontend/src/index.tsx
+++ kpm/kpm-frontend/src/index.tsx
@@ -2,5 +2,8 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { App } from "./app";
 
-const root = ReactDOM.createRoot(document.getElementById("kpm-6cf53")!);
-root.render(<App />);
+// Only mount menu in outermost frame
+if (window.frameElement === null) {
+  const root = ReactDOM.createRoot(document.getElementById("kpm-6cf53")!);
+  root.render(<App />);
+}
