e7372ee93732e970e393326a749b91574699d6ec
Sebastian Ware <sebastian@urbantalk.se>
2023-01-19 16:10:29 +0100

    Improved text on beta activation page (from Martin)

--- kpm/kpm-frontend/src/beta/App.tsx
+++ kpm/kpm-frontend/src/beta/App.tsx
@@ -46,7 +46,27 @@ export function App() {
           Be aware that the project is under active development and you may
           experience bugs and short outages. We will however be treating this as
           a production deployment during the Beta phase and effort will be made
-          to keep it as smooth as possible.
+          to keep it as smooth as possible. Please read more about the project
+          and the beta testing here:
+        </p>
+        <ol>
+          <li>
+            <a href="https://intra.kth.se/en/it/intro/personliga-menyn/utvecklingsprojekt-nya-personliga-menyn-1.1207009">
+              Read more about the project
+            </a>
+          </li>
+          <li>
+            <a href="https://intra.kth.se/en/utbildning/systemstod/nyheter/vi-soker-larare-som-vill-testa-nya-personliga-menyn-1.1211986">
+              Read more about the beta test
+            </a>
+          </li>
+        </ol>
+        <p>
+          If you are missing something or have found a bug or anything else that
+          isn’t working as intended, please send a screenshot and an explanation
+          of the problem to{" "}
+          <a href="mailto:it-support@kth.se">it-support@kth.se</a>. Thank you
+          for participating in the beta development!
         </p>
       </div>
       {isActive ? (
