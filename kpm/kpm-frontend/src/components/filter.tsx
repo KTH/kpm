aa356af45d87cb1dd28fd3a2ed95f33a09c73353
Sebastian Ware <sebastian@urbantalk.se>
2023-01-30 13:59:34 +0100

    Fix typo

--- kpm/kpm-frontend/src/components/filter.tsx
+++ kpm/kpm-frontend/src/components/filter.tsx
@@ -24,7 +24,7 @@ export function FilterOption<T = any>({
 }: TOptionProps<T>) {
   return (
     <li
-      tabindex="0"
+      tabIndex={0}
       className={filter === value ? "active" : undefined}
       onClick={() => onSelect(value)}
     >
