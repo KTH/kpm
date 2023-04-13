e4f52581b66b77bf0607bd5c3ae65004f0b0f7bc
Sebastian Ware <sebastian@urbantalk.se>
2023-04-13 08:36:16 +0200

    Translate body text to Swedish.

--- kpm/kpm-frontend/src/beta/App.tsx
+++ kpm/kpm-frontend/src/beta/App.tsx
@@ -114,39 +114,38 @@ export function App() {
       <h1>Nya personliga menyn (Beta)</h1>
       <div className="lead">
         <p>
-          Welcome to the Beta test of the KTH Personal Menu. You can toggle the
-          new menu on and off by visiting this page.
+          Välkommen till betatestet av den nya personliga menyn. Du kan växla
+          mellan nya och gamla menyn genom att besöka denna sida.
         </p>
       </div>
       <div className="paragraphs">
         <p>
-          Be aware that the project is under active development and you may
-          experience bugs and short outages. We will however be treating this as
-          a production deployment during the Beta phase and effort will be made
-          to keep it as smooth as possible. Please read more about the project
-          and the beta testing here:
+          Tänk på att projektet är under aktiv utveckling och att du kan uppleva
+          buggar och korta störningar. Vi kommer dock att behandla detta som en
+          produktionsmiljö under betatestfasen och vi kommer att göra vårt bästa
+          för att hålla det så smidigt som möjligt. Läs mer om projektet och
+          betatestet här:
         </p>
         <ol>
           <li>
             <a href="https://intra.kth.se/en/utbildning/systemstod/nyheter/nya-personliga-menyn-lanseras-i-tre-steg-1.1221268">
-              The new Personal menu is launched in three steps
+              Nya personliga menyn lanseras i tre steg
             </a>
           </li>
           <li>
             <a href="https://intra.kth.se/en/it/intro/personliga-menyn/utvecklingsprojekt-nya-personliga-menyn-1.1207009">
-              Development Project - New Personal menu
+              Utvecklingsprojekt - Nya personliga menyn
             </a>
           </li>
         </ol>
         <p>
-          If you are missing something or have found a bug or anything else that
-          isn’t working as intended, please send a screenshot and an explanation
-          of the problem to{" "}
+          Om du saknar något eller har hittat en bugg eller något annat som inte
+          fungerar som det ska, vänligen skicka en skärmdump och en förklaring
+          av problemet till{" "}
           <a href="mailto:it-support@kth.se?subject='KTH Personliga Meny BETA -- Feedback'">
             it-support@kth.se
           </a>
-          . Please note that you might need to reactivate the beta during the
-          testing. Thank you for participating in the beta development!
+          . Tack för att du deltar i betatestet!
         </p>
       </div>
       {isActive ? (
