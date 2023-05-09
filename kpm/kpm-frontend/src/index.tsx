c066cd343a55c6da62039986c04a8399544af75f
Rasmus Kaj <kaj@kth.se>
2023-05-09 11:50:34 +0200

    Send kpm loaded event when authstate changes.

--- kpm/kpm-frontend/src/index.tsx
+++ kpm/kpm-frontend/src/index.tsx
@@ -1,11 +1,28 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { App } from "./app";
-import { initSessionCheck } from "./state/authState";
+import { authState, initSessionCheck } from "./state/authState";
+import { TPubSubEvent } from "./state/PubSub";
+
+function sendKpmLoaded(authorized: boolean) {
+  document.dispatchEvent(
+    new CustomEvent("kpmLoaded", {
+      detail: {
+        authorized,
+        lang: window.__kpmSettings__?.["lang"] || "en",
+      },
+    })
+  );
+}
 
 // Only mount menu in outermost frame
 if (window.frameElement === null) {
   const root = ReactDOM.createRoot(document.getElementById("kpm-6cf53")!);
+  authState.subscribe((event: TPubSubEvent<any>) => {
+    if (event.name === "CurrentUser") {
+      sendKpmLoaded(!!event.value);
+    }
+  });
   initSessionCheck();
   root.render(<App />);
 }
