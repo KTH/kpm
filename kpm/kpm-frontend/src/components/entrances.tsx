787010438abbab4ca98eea3d80b95acbed86aa60
Carlos Saito <carlos@saito.style>
2024-04-03 12:17:00 +0200

    Identify current site

--- kpm/kpm-frontend/src/components/entrances.tsx
+++ kpm/kpm-frontend/src/components/entrances.tsx
@@ -2,12 +2,37 @@ import React from "react";
 import { i18n } from "../i18n/i18n";
 import "./entrances.scss";
 
+const STUDENT_REGEX1 = /^https?:\/\/student\.kth\.se/;
+const STUDENT_REGEX2 = /^https?:\/\/(www\.)?kth.se\/student($|\/)/;
+const EXTERNAL_REGEX = /^https?:\/\/(www\.)?kth.se\/(?!student($|\/))/;
+const INTRA_REGEX = /^https?:\/\/intra\.kth\.se/;
+
+const labels = {
+  external: "kth.se",
+  intra: i18n("Intranet"),
+  student: i18n("Student web"),
+};
+
+/** Get the current site */
+function getCurrentSite(): "external" | "intra" | "student" | undefined {
+  const currentPage = window.location.toString();
+
+  if (STUDENT_REGEX1.test(currentPage)) {
+    return "student";
+  } else if (INTRA_REGEX.test(currentPage)) {
+    return "intra";
+  } else if (EXTERNAL_REGEX.test(currentPage)) {
+    return "external";
+  }
+}
+
 /**
  * Component "<Entrances />"
  * Shortcuts to one of the three "KTH sites" (kth.se, student web, intranet)
  */
 export function Entrances() {
   const [expanded, setExpanded] = React.useState(false);
+  const currentSite = getCurrentSite();
 
   return (
     <React.Fragment>
@@ -18,7 +43,7 @@ export function Entrances() {
             setExpanded(!expanded);
           }}
         >
-          kth.se
+          {currentSite ? labels[currentSite] : labels["external"]}
         </button>
         {expanded && (
           <div className="kpm-mini-dialog">
@@ -43,17 +68,29 @@ export function Entrances() {
       >
         <ul>
           <li>
-            <a href="https://kth.se" className="kth-menu-item">
+            <a
+              href="https://kth.se"
+              className="kth-menu-item"
+              aria-current={currentSite === "external"}
+            >
               kth.se
             </a>
           </li>
           <li>
-            <a href="https://kth.se/student" className="kth-menu-item">
+            <a
+              href="https://kth.se/student"
+              className="kth-menu-item"
+              aria-current={currentSite === "student"}
+            >
               {i18n("Student web")}
             </a>
           </li>
           <li>
-            <a href="https://intra.kth.se" className="kth-menu-item">
+            <a
+              href="https://intra.kth.se"
+              className="kth-menu-item"
+              aria-current={currentSite === "intra"}
+            >
               {i18n("Intranet")}
             </a>
           </li>
