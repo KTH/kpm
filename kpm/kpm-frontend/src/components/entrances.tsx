e2f59c945befc81fd4057a22ceb44f2f4f32c226
Carlos Saito <carlos@saito.style>
2024-04-03 12:04:53 +0200

    Add entrances for mobile

--- kpm/kpm-frontend/src/components/entrances.tsx
+++ kpm/kpm-frontend/src/components/entrances.tsx
@@ -7,10 +7,35 @@ import "./entrances.scss";
  * Shortcuts to one of the three "KTH sites" (kth.se, student web, intranet)
  */
 export function Entrances() {
+  const [expanded, setExpanded] = React.useState(false);
+
   return (
     <React.Fragment>
-      <nav className="kpm-entrances-expandable">
-        <button className="kth-menu-item dropdown">kth.se</button>
+      <nav className="kpm-entrances-expandable" aria-label={i18n("Websites")}>
+        <button
+          className="kth-menu-item dropdown"
+          onClick={() => {
+            setExpanded(!expanded);
+          }}
+        >
+          kth.se
+        </button>
+        {expanded && (
+          <div className="kpm-mini-dialog">
+            <h2 id="kpm-entrances-mobile-title">{i18n("Websites")}</h2>
+            <ul>
+              <li>
+                <a href="https://kth.se">kth.se</a>
+              </li>
+              <li>
+                <a href="https://kth.se/student">{i18n("Student web")}</a>
+              </li>
+              <li>
+                <a href="https://intra.kth.se">{i18n("Intranet")}</a>
+              </li>
+            </ul>
+          </div>
+        )}
       </nav>
       <nav
         className="kth-entrances kpm-entrances"
